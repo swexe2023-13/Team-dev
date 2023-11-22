e21dbec45bee6a42054be31a51191711bee61aa5
teshibayuzu <2111140026r@ed.fuk.kindai.ac.jp>
2023-11-22 06:52:10 +0000

    css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "d29d811d7d7f41fab0415ddb97a4013a.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "642ec093cf6a492399df2764a3fd44a7.vfs.cloud9.us-east-1.amazonaws.com"
 end
